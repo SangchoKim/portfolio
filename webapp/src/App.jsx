d330cef48ecb22c1b552dab3031b9c8771985c5d
SangchoKim <wjdrms1919@gmail.com>
2021-04-13 00:18:00 +0900

    feat: structFolder 1. install react-query 2. Acompose AppLayout 3. compose App, index

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,7 +1,40 @@
-import './App.css';
+import React from 'react';
+import { Global, css } from '@emotion/react';
+import { Route, Switch } from 'react-router-dom';
+import AppLayout from './components/AppLayout';
+// import Sidebar from './components/Sidebar';
+import normalize from 'normalize.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Switch>
+        <Route path={['/', '/backtests/:id', '/workspace', '/explore']} exact>
+          <AppLayout>
+            <AppLayout.Side>{/* <Sidebar /> */}</AppLayout.Side>
+            <AppLayout.Main>
+              {/* <Switch>
+              <Route path={['/', '/lab', '/backtests/:id']} exact>
+                <Lab />
+              </Route>
+              <Route path="/workspace">
+                <Workspace />
+              </Route>
+              <Route path="/explore">
+                <Explore />
+              </Route>
+            </Switch> */}
+            </AppLayout.Main>
+          </AppLayout>
+        </Route>
+      </Switch>
+      <Global styles={globalStyle} />
+    </>
+  );
 }
+
+const globalStyle = css`
+  ${normalize}
+`;
 
 export default App;
